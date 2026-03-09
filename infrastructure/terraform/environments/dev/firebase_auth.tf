c42b745027a5a28c1dfb3a026731ff3537db774e
Alex Brandt <alunduil@gmail.com>
2026-03-09 20:49:15 +0000

    feat(infra): add Google OAuth identity provider config (#36)
    - Add data sources to read OAuth credentials from Secret Manager
    - Add google_identity_platform_default_supported_idp_config for google.com
    - Remove TODO placeholder now that secrets are populated
    
    Closes #36

--- infrastructure/terraform/environments/dev/firebase_auth.tf
+++ infrastructure/terraform/environments/dev/firebase_auth.tf
@@ -35,6 +35,29 @@ resource "google_identity_platform_config" "default" {
   ]
 }
 
-# TODO(#36): Add google_identity_platform_default_supported_idp_config for
-# google.com after OAuth credentials are stored in Secret Manager.
-# Requires a second apply once the manual steps are complete.
+# Read the Google OAuth client ID from Secret Manager.
+data "google_secret_manager_secret_version" "firebase_auth_google_client_id" {
+  project = var.gcp_dev_project_id
+  secret  = google_secret_manager_secret.firebase_auth_google_client_id.secret_id
+  version = "latest"
+}
+
+# Read the Google OAuth client secret from Secret Manager.
+data "google_secret_manager_secret_version" "firebase_auth_google_client_secret" {
+  project = var.gcp_dev_project_id
+  secret  = google_secret_manager_secret.firebase_auth_google_client_secret.secret_id
+  version = "latest"
+}
+
+# Configure Google as a supported identity provider.
+resource "google_identity_platform_default_supported_idp_config" "google" {
+  project = var.gcp_dev_project_id
+
+  idp_id        = "google.com"
+  client_id     = data.google_secret_manager_secret_version.firebase_auth_google_client_id.secret_data
+  client_secret = data.google_secret_manager_secret_version.firebase_auth_google_client_secret.secret_data
+
+  enabled = true
+
+  depends_on = [google_identity_platform_config.default]
+}
